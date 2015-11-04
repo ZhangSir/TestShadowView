62983e8049613363a5f745bbf06447a793cd2ded
Zhang Sir <super_spy_happy@163.com>
2015-11-04 10:53:59 +0800

    提交另外一种实现方式

--- gen/com/test/shadowview/R.java
+++ gen/com/test/shadowview/R.java
@@ -982,8 +982,9 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int bg_circle_blue_shadow=0x7f020057;
-        public static final int bg_retangle_red_shadow=0x7f020058;
-        public static final int ic_launcher=0x7f020059;
+        public static final int bg_circle_green_shadow=0x7f020058;
+        public static final int bg_retangle_red_shadow=0x7f020059;
+        public static final int ic_launcher=0x7f02005a;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -997,12 +998,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003f;
+        public static final int action_settings=0x7f050040;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int btn1=0x7f05003d;
         public static final int btn2=0x7f05003e;
+        public static final int btn3=0x7f05003f;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
